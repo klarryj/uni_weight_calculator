1d929cc76c640e662f895d957583e9c30a1aadc1
klarryj <lpcft01@gmail.com>
2026-03-22 17:08:37 +0300

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -16,12 +16,6 @@ OLEVEL_WEIGHTS = {
     "PASS": 0.1,
 }
 
-WEIGHT_MULTIPLIERS = {
-    "ESSENTIAL": 3,
-    "RELEVANT": 2,
-    "DESIRABLE": 1,
-}
-
 CLASSIFICATION = {
     "SAFE_MARGIN": 1.0,
     "BORDERLINE_MARGIN": -0.5,
@@ -31,4 +25,77 @@ SCHEME_LABELS = {
     "DQ": "District Quota",
     "GS": "Government Sponsorship",
     "PS": "Private Sponsorship",
+}
+
+SUBJECT_ALIASES = {
+    "math": "Mathematics",
+    "mathematics": "Mathematics",
+    "subsidiary mathematics": "Sub-Mathematics",
+    "sub mathematics": "Sub-Mathematics",
+    "sub-mathematics": "Sub-Mathematics",
+    "sub maths": "Sub-Mathematics",
+    "sub math": "Sub-Mathematics",
+    "physics": "Physics",
+    "chemistry": "Chemistry",
+    "biology": "Biology",
+    "economics": "Economics",
+    "geography": "Geography",
+    "history": "History",
+    "literature": "Literature",
+    "literature in english": "Literature",
+    "entrepreneurship": "Entrepreneurship",
+    "computer studies": "Computer Studies",
+    "ict": "ICT",
+    "information and communication technology": "ICT",
+    "general paper": "General Paper",
+    "gp": "General Paper",
+    "fine art": "Fine Art",
+    "art": "Fine Art",
+    "agriculture": "Agriculture",
+    "food and nutrition": "Food and Nutrition",
+    "islamic religious education": "Islamic Religious Education",
+    "ire": "Islamic Religious Education",
+    "christian religious education": "Christian Religious Education",
+    "cre": "Christian Religious Education",
+    "divinity": "Divinity",
+    "kiswahili": "Kiswahili",
+    "french": "French",
+    "german": "German",
+    "luganda": "Luganda",
+    "runyakitara": "Runyakitara",
+    "music": "Music",
+    "physical education": "Physical Education",
+}
+
+CANONICAL_SUBJECTS = sorted(set(SUBJECT_ALIASES.values()))
+
+GRADE_ALIASES = {
+    "A": "A",
+    "B": "B",
+    "C": "C",
+    "D": "D",
+    "E": "E",
+    "O": "O",
+    "F": "F",
+    "a": "A",
+    "b": "B",
+    "c": "C",
+    "d": "D",
+    "e": "E",
+    "o": "O",
+    "f": "F",
+}
+
+GENDER_ALIASES = {
+    "male": "Male",
+    "m": "Male",
+    "female": "Female",
+    "f": "Female",
+}
+
+CITIZENSHIP_ALIASES = {
+    "ugandan": "Ugandan",
+    "non-ugandan": "Non-Ugandan",
+    "non ugandan": "Non-Ugandan",
+    "foreign": "Non-Ugandan",
 }
